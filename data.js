a45ec266972cf10d5c6936c4d24bc49238700b78
Eduardo Lemus Mendoza <79598767+317279809@users.noreply.github.com>
2024-04-29 14:06:34 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,103 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1714418962130",
+      "name": "1714418962130",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.8953408819753186,
+        "pitch": 0.24662927485670672,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6576926666282823,
+          "pitch": 0.12320720871476709,
+          "rotation": 0,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.051476486872827465,
+          "pitch": 0.13510013851649383,
+          "title": "Pino seco",
+          "text": "Agua Insuficiente"
+        },
+        {
+          "yaw": 1.065660592511911,
+          "pitch": 0.021313187492486563,
+          "title": "Otro pino seco",
+          "text": "Agua Insuficiente"
+        }
+      ]
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.05646354209113014,
+        "pitch": 0.35195758134454813,
+        "fov": 1.4404666184485553
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1360920235044354,
+          "pitch": 0.36401841583580463,
+          "rotation": 0,
+          "target": "0-1714418962130"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.047491425365812745,
+          "pitch": 0.29384261538225687,
+          "title": "WTF",
+          "text": "Sepa donde es esto"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
